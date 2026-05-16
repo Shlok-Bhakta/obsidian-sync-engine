cee927139a3d83c9ab6950605712d93d7174948f
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-05-16 15:43:26 -0500

    complete basic migration runner

--- server/src/db/MigrationRunner.ts
+++ server/src/db/MigrationRunner.ts
@@ -1,13 +1,23 @@
-import { sql } from "bun";
+import { sql, Glob } from "bun";
+import migration0001 from "./migrations/0001_init.sql" with { type: "file" };
 
+const migrationsManifest = [
+    {
+        name: "0001_init",
+        sql: migration0001,
+    }
+]
 
 
 export async function bootstrapDB() {
     console.log("bootstrapping db");
     // if this boi runs then the db is ok to run;
     await canConnect();
-    // check to see if our migrations db exists
+    // check and create our migrations table
     await setupDB();
+    // check to see if there are any migrations to run
+    await applyMigrations();
+
 
 }
 
@@ -19,13 +29,21 @@ async function canConnect() {
     } else {
         throw new Error("something somewhere is goofed up. Could not run SELECT 1; on the db");
     }
+
 }
 
 async function setupDB() {
     const migrationsTableName = "migrations";
     // check to see if migrations table exists
-    let result = await sql`SELECT * FROM ${migrationsTableName};`.values();
-    if (result.length > 0) {
+    console.log("checking if migrations table exists");
+    let exists = false;
+    try {
+    await sql.unsafe(`SELECT * FROM ${migrationsTableName};`).values();
+    exists = true;
+    } catch (e) {
+        exists = false;
+    }
+    if (exists) {
         console.log("migrations table exists");
         return;
     }else{
@@ -35,11 +53,32 @@ async function setupDB() {
             name VARCHAR(255) NOT NULL,
             created_at TIMESTAMP NOT NULL);`
         );
-    }
-
-    
+    } 
 }
 
+type MigrationRow = { name: string };
+async function applyMigrations() {
+    console.log("applying migrations");
+
+    // get all migrations that have already been run
+    let migrations = await sql<MigrationRow[]>`SELECT name FROM migrations;`;
+    let migrationsNames: string[] = migrations.map(m => m.name);
+    console.log("migrations", migrationsNames);
+
+    for (const migration of migrationsManifest) {
+        // first check to see if the migration has already been applied
+        if (migrations.find(m => m.name === migration.name)) {
+            console.log("migration already applied");
+            continue;
+        }
+        console.log("applying migration", migration.name);
+        // console.log("sql", await Bun.file(migration.sql).text());
+        await sql.unsafe(await Bun.file(migration.sql).text());
+        console.log("applied migration", migration.name);
+        await sql`INSERT INTO migrations (name, created_at) VALUES (${migration.name}, NOW());`;
+        console.log("inserted migration", migration.name);
+    }
+}
 // export function isMigrationsSetup(db: sql.Database, migrations: string[]) {
 
 // }
--- server/src/db/migrations/0001_init.sql
+++ server/src/db/migrations/0001_init.sql
@@ -1,1 +1,20 @@
-CREATE TABLE 
+-- Migration creates a table to track the files/folders
+CREATE TABLE IF NOT EXISTS files (
+    id BIGSERIAL PRIMARY KEY,
+    path TEXT NOT NULL UNIQUE,
+    hash TEXT, -- enforce in application code that this hash exists on everything except folders
+    is_folder BOOLEAN NOT NULL,
+    is_yjs BOOLEAN NOT NULL,
+    deleted_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- only stores one value, meant to be used to know wether we need to ask the client "pretty please send me the entire file tree and content you have so I can have it setup for others"
+CREATE TABLE IF NOT EXISTS client_bootstrapped (
+    id SERIAL PRIMARY KEY,
+    bootstrapped BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- actually create the row`
+INSERT INTO client_bootstrapped (id, bootstrapped) VALUES (1, FALSE) ON CONFLICT (id) DO NOTHING;
