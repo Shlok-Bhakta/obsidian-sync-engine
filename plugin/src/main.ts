f976ed3718bc9f73bb0851206fae57d723fcda8b
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-06-27 23:20:40 -0500

    basic auth test

--- plugin/src/main.ts
+++ plugin/src/main.ts
@@ -16,77 +16,120 @@ import {
 
 export default class MyPlugin extends Plugin {
 	settings!: MyPluginSettings;
+	ws: WebSocket | null = null;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
+		// connect to server over ws
+		this.ws = new WebSocket('ws://obsidian-hono.shloklab.us/ws');
+		
+
+		this.ws.onopen = () => {
+			console.log('Connected to server');
+			if(this.ws){
+			this.ws.send(
+				JSON.stringify({
+					type: 'auth',
+						token: '88c53ab8-c8b7-4e6d-9481-f1d4326e4cfb',
+					}),
+				);
+			}
+		};
+
+		// // This creates an icon in the left ribbon.
 		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			if(this.ws){
+			this.ws.send(
+				JSON.stringify({
+					type: 'message',
+					message: 'Hello from Obsidian',
+				}),
+			);
+		}
 		});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+		this.ws.onmessage = (event) => {
+			console.log(event.data);
+			const data = JSON.parse(event.data.toString());
+			if (data.type === 'message_received') {
+				new Notice(data.message);
+			}
+		};
+		this.ws.onclose = () => {
+			console.log('Disconnected from server');
+		};
+		this.ws.onerror = (error) => {
+			console.error('Error: ', error);
+		};
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status bar text');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			},
-		});
+		// // This adds a simple command that can be triggered anywhere
+		// this.addCommand({
+		// 	id: 'open-modal-simple',
+		// 	name: 'Open modal (simple)',
+		// 	callback: () => {
+		// 		new SampleModal(this.app).open();
+		// 	},
+		// });
+		// // This adds an editor command that can perform some operation on the current editor instance
+		// this.addCommand({
+		// 	id: 'replace-selected',
+		// 	name: 'Replace selected content',
+		// 	editorCallback: (
+		// 		editor: Editor,
+		// 		_ctx: MarkdownView | MarkdownFileInfo,
+		// 	) => {
+		// 		editor.replaceSelection('Sample editor command');
+		// 	},
+		// });
+		// // This adds a complex command that can check whether the current state of the app allows execution of the command
+		// this.addCommand({
+		// 	id: 'open-modal-complex',
+		// 	name: 'Open modal (complex)',
+		// 	checkCallback: (checking: boolean) => {
+		// 		// Conditions to check
+		// 		const markdownView =
+		// 			this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 		if (markdownView) {
+		// 			// If checking is true, we're simply "checking" if the command can be run.
+		// 			// If checking is false, then we want to actually perform the operation.
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
+
+		// 			// This command will only show up in Command Palette when the check function returns true
+		// 			return true;
+		// 		}
+		// 		return false;
+		// 	},
+		// });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
-			new Notice('Click');
-		});
+		// this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
+		// 	new Notice('Click');
+		// });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
-		);
+		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		// this.registerInterval(
+		// 	window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
+		// );
 	}
 
-	onunload() {}
+	onunload() {
+		if(this.ws){
+			this.ws.close();
+			this.ws = null;
+		}
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,60 @@
 import { Hono } from 'hono'
+import { upgradeWebSocket, websocket } from 'hono/bun'
+import { bootstrapDB } from './db/MigrationRunner'
 
+bootstrapDB();
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+const AUTH_TOKEN = "88c53ab8-c8b7-4e6d-9481-f1d4326e4cfb";
+app.get(
+  '/ws',
+  upgradeWebSocket((c) => {
+    let authed = false;
+    return {
+      onOpen(_event, ws){
+        ws.send(JSON.stringify({
+          type: "auth_required"
+        }));
+      },
+      onMessage(event, ws) {
+        // ask client to auth 
+        console.log(`Message from client: ${event.data}`)
+        if(!authed){
+          const data = JSON.parse(event.data.toString());
+          if(data.type === "auth" && data.token === AUTH_TOKEN){
+            authed = true;
+            ws.send(JSON.stringify({
+              type: "auth_success"
+            }));
+          } else {
+            ws.send(JSON.stringify({
+              type: "auth_failed"
+            }));
+            ws.close(1000, "Unauthorized");
+            return;
+          }
+        }else{
+          const data = JSON.parse(event.data.toString());
+          if(data.type === "message"){
+            ws.send(JSON.stringify({
+              type: "message_received",
+              message: data.message
+            }));
+          }
+        }
+      },
+      onClose: () => {
+        console.log('Connection closed')
+      },
+    }
+  })
+)
 
-export default app
+export default {
+  fetch: app.fetch,
+  websocket,
+  maxRequestBodySize: 1024 * 1024 * 10, // 10MB
+};
